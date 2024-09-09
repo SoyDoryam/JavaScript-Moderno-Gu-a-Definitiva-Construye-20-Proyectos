ff8931f48f04f77a07fc76e5e728b7f7912f3b1f
Doryam <rizo0655@gmail.com>
2024-09-09 14:17:02 -0600

    Todo lo que debes saber de las variables con var

---
+++ 02-Variables/js/app01.js
@@ -0,0 +1,21 @@
+// Inicializar una variable con un valor
+var producto = "Monitos de 24 Pulgadas";
+console.log(producto);
+
+// Las variables se pueden reacsignar
+producto = "Monitos de 19 Pulgadas";
+console.log(producto);
+
+//JavaScrip  es un lenguaje de tipo dinamico, no se especifica tipo de datso
+producto = 20;
+console.log(producto);
+
+//se peuden inicializar sin valor y asignar despues
+var disponible;
+disponible = true;
+console.log(disponible);
+
+//Inicializar multiple variables
+var categoria = "Computadoras";
+var marca = "Marca famosa";
+var calificaciones = 5;
