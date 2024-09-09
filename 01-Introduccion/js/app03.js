1c7923d43baeeb5a399c16400c69394b76494b8c
Doryam <rizo0655@gmail.com>
2024-09-09 13:25:33 -0600

    Consideraciones en JavaScript con el ;

---
+++ 01-Introduccion/js/app03.js
@@ -0,0 +1,18 @@
+//Ejemplo sin punto y coma:
+console.log("Hola");
+console.log("Mundo");
+//Ejemplo con punto y coma:
+console.log("Hola");
+console.log("Mundo");
+
+// Ejemplo de función bien ordenada:
+function hola() {
+  console.log("Hola");
+  console.log("Mundo");
+}
+
+// Ejemplo de función desordenada:
+function hola() {
+  console.log("Hola");
+  console.log("Mundo");
+}
