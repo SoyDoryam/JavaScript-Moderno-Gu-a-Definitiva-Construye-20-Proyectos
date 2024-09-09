410cbc6d828c41c0354309b3b34adbeb8ea941a8
Doryam <rizo0655@gmail.com>
2024-09-09 14:25:36 -0600

    Todo lo que debes saber de las variables con let

---
+++ 02-Variables/js/app02.js
@@ -0,0 +1,15 @@
+// Inicializar una variable con let
+let producto = "Tablet";
+
+// Reasignar el valor
+producto = "Monitor"; // Cambio a un nuevo valor de tipo string
+producto = 20; // Cambio a un valor de tipo number
+producto = true; // Cambio a un valor de tipo boolean
+producto = null; // Cambio a un valor nulo
+
+console.log(producto); // Imprime el último valor asignado (null)
+
+// Inicializar una variable sin valor
+let precio;
+precio = 300; // Asigna un valor a la variable precio
+console.log(precio); // Imprime el valor de precio (300)
