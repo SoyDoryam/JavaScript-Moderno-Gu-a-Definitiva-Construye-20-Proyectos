dcac44c48427e22bec6ead33f9a9b1dc1d8e24dd
Doryam <rizo0655@gmail.com>
2024-09-09 12:54:47 -0600

    Comentar el Código en JavaScript

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,4 +1,9 @@
-const nombre = prompt("Cual es tu nombre ");
+const nombre = prompt("Cual es tu nombre:");
+
+//toma el nombre y lo muestra en pantalla
 document.querySelector(
   ".contenido"
 ).innerHTML = `${nombre} esta aprendiendo JavaScript Moderno`;
+
+//asigna el valor hacia la variable producto
+const producto = `Monitor de 24 pulgadas`;
