2d16c89d44059d605dabf6822b021412036b63b1
Doryam <rizo0655@gmail.com>
2024-09-13 23:09:55 -0600

    String Methods - Convertir a Mayusculas o Minusculas

---
+++ 03-Strings/js/app07.js
@@ -0,0 +1,11 @@
+// .toUpperCase() te va a permitir cambiar un texto a mayúsculas
+const producto = "Monitor 24 pulgadas ";
+console.log(producto.toUpperCase()); // Convierte el texto a mayúsculas: "MONITOR 24 PULGADAS"
+
+// .toLowerCase() convierte un texto a minúsculas
+console.log(producto.toLowerCase()); // Convierte el texto a minúsculas: "monitor 24 pulgadas"
+
+// .toString() convierte un número a una cadena de texto
+const cantidad = 200;
+console.log(cantidad); // Muestra el número: 200
+console.log(cantidad.toString()); // Convierte el número a string: "200"
