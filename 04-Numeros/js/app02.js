796be520f424062f1ecf32bbe976cf4010dcec5a
Doryam <rizo0655@gmail.com>
2024-09-20 18:59:24 -0600

    Operaciones en JavaScript

---
+++ 04-Numeros/js/app02.js
@@ -0,0 +1,20 @@
+const numero1 = 30;
+const numero2 = 20;
+
+let resultado;
+
+// Suma
+resultado = numero1 + numero2;
+console.log(`${numero1} + ${numero2} = ${resultado}`);
+// Resta
+resultado = numero1 - numero2;
+console.log(`${numero1} - ${numero2} = ${resultado}`);
+// Multiplicacion
+resultado = numero1 * numero2;
+console.log(`${numero1} * ${numero2} = ${resultado}`);
+// Division
+resultado = numero1 / numero2;
+console.log(`${numero1} / ${numero2} = ${resultado}`);
+// Modulo
+resultado = numero1 % numero2;
+console.log(`${numero1} % ${numero2} = ${resultado}`);
