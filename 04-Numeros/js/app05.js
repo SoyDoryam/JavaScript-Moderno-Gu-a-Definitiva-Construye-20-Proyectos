ec1b4ddea12944640748bf82e3dc08ee3c01530c
Doryam <rizo0655@gmail.com>
2024-09-22 17:05:28 -0600

    Incrementos o Decrementos

---
+++ 04-Numeros/js/app05.js
@@ -0,0 +1,12 @@
+// Veamos como ir incrmentando un valor de 1 en 1 o en determinado cantidad
+// incluir incremento por 1 y menos de 1
+
+let puntaje = 5;
+
+puntaje++;
+puntaje--;
+
+++puntaje;
+--puntaje;
+
+console.log(puntaje);
