f716296bf9ce1ab27b53cf7c26dcf93b1d676df6
Doryam <rizo0655@gmail.com>
2024-09-22 18:06:06 -0600

    Buenas practicas a la hora de evaluar un Booleans

---
+++ 06-Booleans/js/app03.js
@@ -0,0 +1,13 @@
+// Existe otra forma de comparar si un valor es verdadero y es por medio de algo llamado un operador ternario
+
+const boolean1 = true;
+const boolean2 = false;
+
+if (boolean1) {
+  console.log("si es true");
+} else {
+  console.log("no, no es true");
+}
+
+//El c+¦digo anterior es f+ícil de leer no?, pero se puede simplificar un poco m+ís
+console.log(boolean1 ? "Si es true" : "No no es true");
