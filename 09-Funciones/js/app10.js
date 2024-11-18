6909f89828e6e1c4268b8a8a64e6bec035647c4d
Doryam <rizo0655@gmail.com>
2024-11-17 22:40:22 -0600

    Arrow Functions

---
+++ 09-Funciones/js/app10.js
@@ -0,0 +1,14 @@
+const aprendiendo = function () {
+  console.log("1. Aprendiendo JavaScript");
+};
+
+const aprendiendo2 = () => {
+  return `2. Aprendiendo JavaScript`;
+};
+
+//si la funcon tiene un alinea las llaves y el return son opcionales
+const aprendiendo3 = () => ` 3. Aprendiendo JavaScript`;
+
+console.log(aprendiendo());
+console.log(aprendiendo2());
+console.log(aprendiendo3());
