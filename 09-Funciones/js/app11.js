9cf9982294beb8de97742a0f81b3abddfb2b7e0a
Doryam <rizo0655@gmail.com>
2024-11-17 22:46:11 -0600

    Las Ventajas de los Arrow Functions

---
+++ 09-Funciones/js/app11.js
@@ -0,0 +1,10 @@
+const aprendiendo = function (tecnologias, tecnologias2) {
+  console.log(`1. Aprendiendo ${tecnologias} y ${tecnologias2}`);
+};
+
+aprendiendo("JavaScript", "Node.js");
+
+const aprendiendo2 = (tecnologia, tecnologias2) =>
+  `2. Aprendiendo ${tecnologia} y ${tecnologias2}`;
+
+console.log(aprendiendo2("JavaScript", "Node.js"));
