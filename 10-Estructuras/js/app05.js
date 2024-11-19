c3af7cd89f736e4c0d250e6da7de8c543f9a93b2
Doryam <rizo0655@gmail.com>
2024-11-19 02:28:23 -0600

    Switch para evaluar múltiples Condiciones

---
+++ 10-Estructuras/js/app05.js
@@ -0,0 +1,24 @@
+//Swicht case
+
+const metodoPago = "efectivo";
+
+switch (metodoPago) {
+  case "efectivo":
+    pagar();
+    break;
+  case "cheque":
+    console.log(`Pagaste con ${metodoPago}`);
+    break;
+  case "tarjeta":
+    console.log(`Pagaste con ${metodoPago}`);
+    break;
+  default:
+    console.log(
+      `Aun no has selecionado un metodo de pago o metodo de pago no soportado`
+    );
+    break;
+}
+
+function pagar() {
+  console.log("Pagando...");
+}
