7031c87ca2310e6f6f35b05443f0228f1753a270
Doryam <rizo0655@gmail.com>
2024-11-19 01:14:29 -0600

    Comparando Mayor o Menor que

---
+++ 10-Estructuras/js/app03.js
@@ -0,0 +1,10 @@
+// Operador mayor que menor que
+
+const dinero = 300;
+const totalPagar = 300;
+
+if (dinero >= totalPagar) {
+  console.log("si podemos pagar.");
+} else {
+  console.log("Fondos Insuficientes");
+}
